ab913e8191652098746d7b230c71dfebb9303cfa
Doan Thi Anh Ly <doananhly1999@gmail.com>
2023-09-24 15:47:08 +0700

    configre eb

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
 
-eb use lyly-udagram-api-dev
+eb use Lyly-udagram-api-dev
 
 eb setenv POSTGRES_HOST=$POSTGRES_HOST \
           POSTGRES_DB=$POSTGRES_DB  \
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://lyly-dagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Lyly-udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
